501edec165e24ed3ef2fc17483969f638d20bd9a
Дмитрий Лущан <dluschan@gmail.com>
2016-12-02 20:51:57 +0300

    исправлена задача «Укладка плитки»

--- vseros/2014-15/reg/platelanding.py
+++ vseros/2014-15/reg/platelanding.py
@@ -1,3 +1,7 @@
+# каждая буква хранит количество вариатов раскладок из n элементов, раскладка перебирается с буквы X
+# | a: 0 X | b: 0   | c: 0 X | d: 0     |
+# |    0 0 |    0 X |    0   |    0 0 X |
+
 n, k = [int(q) for q in input().split()]
 hole = []
 for i in range(k):
@@ -9,28 +13,14 @@ a = [0 for i in range(n+1)]
 b = [0 for i in range(n+1)]
 c = [0 for i in range(n+1)]
 d = [0 for i in range(n+1)]
-a[1] = 0
-a[2] = 2
 b[1] = 1
-b[2] = 0
 c[1] = 1
-c[2] = 0
-d[1] = 0
-d[2] = 2
-for i in range(1, 3):
-    if i in hole:
-        #исправить!
-        a[i] = 0
-        b[i] = 0
-        c[i] = 0
-        d[i] = 0
-        
+a[2] = 1 if any(index in hole for index in [1, 2]) else 2
+d[2] = 1 if any(index in hole for index in [1, 3]) else 2
+
 for i in range(3, n+1):
-    if not (i in hole):
-        a[i] = (d[i-2] if (i-2) not in hole else 0) + (a[i-2] if (i-1) not in hole else 0) + b[i-1]
-        b[i] = a[i-1] + (c[i-2] if (i-2) not in hole else 0)
-    
-    if (i+1 not in hole):
-        c[i] = (b[i-2] if (i-1) not in hole else 0) + a[i-1]
-        d[i] = (a[i-2] if (i-1) not in hole else 0) + b[i-1]
-print(a, b, c, d)
+    a[i] = b[i-1] + (0 if any(index in hole for index in [i-2, i]) else d[i-2]) + (0 if any(index in hole for index in [i-1, i]) else a[i-2])
+    b[i] = a[i-1] + (0 if any(index in hole for index in [i-2, i]) else c[i-2])
+    c[i] = a[i-1] + (0 if any(index in hole for index in [i-1, i+1]) else b[i-2])
+    d[i] = b[i-1] + (0 if any(index in hole for index in [i-1, i+1]) else a[i-2])
+print(a[n])
