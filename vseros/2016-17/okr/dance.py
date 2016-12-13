149c7b3efa207795f44af6ed2cf998b977dc1458
Дмитрий Лущан <dluschan@gmail.com>
2016-12-14 02:46:05 +0300

    исправлена ошибка в арифметике номеров

--- vseros/2016-17/okr/dance.py
+++ vseros/2016-17/okr/dance.py
@@ -1,7 +1,6 @@
 n = int(input())
 p = int(input())
 k = int(input()) % n
-
-a = ((((p - 1) - ((p - 1) % 2 * 2 - 1) * k) % n - 1) % n - (p % 2 * 2 - 1) * k) % n + 1
-b = ((((p - 1) - ((p - 1) % 2 * 2 - 1) * k) % n + 1) % n - (p % 2 * 2 - 1) * k) % n + 1
+a = (p - 2 + (p % 2 * 2 - 1) * 2 * k) % n + 1
+b = (p     + (p % 2 * 2 - 1) * 2 * k) % n + 1
 print(min(a, b), max(a, b))
