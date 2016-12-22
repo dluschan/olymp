4a7ba89c5ee3d1542d63ea03f28eea81ae6ef225
Дмитрий Лущан <dluschan@gmail.com>
2016-12-23 00:42:42 +0300

    реализация, которая использует больше памяти, но должна быть быстрее

--- vseros/2016-17/okr/3.py
+++ vseros/2016-17/okr/3.py
@@ -1,15 +1,15 @@
 #решение задачи с помощью интерпретирования пар как 26-ричных чисел
 
 s1 = input()
-g1 = [0 for i in range(26**2)]
 s2 = input()
-g2 = set()
+g1 = [0 for i in range(26**2)]
+g2 = [0 for i in range(26**2)]
 for i in range(len(s1) - 1):
     g1[26 * (int(s1[i], 36) - int('A', 36)) + (int(s1[i+1], 36) - int('A', 36))] += 1
 for i in range(len(s2) - 1):
-    g2.add(26 * (int(s2[i], 36) - int('A', 36)) + (int(s2[i+1], 36) - int('A', 36)))
+    g2[26 * (int(s2[i], 36) - int('A', 36)) + (int(s2[i+1], 36) - int('A', 36))] = 1
 sum = 0
 for i in range(26**2):
-    if i in g2:
+    if g2[i] == 1:
         sum += g1[i]
 print(sum)
