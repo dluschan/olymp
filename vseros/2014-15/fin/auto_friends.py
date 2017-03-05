2a77a5c2d07544bc13d38f0aba34b046e3432834
Dmitry Luschan <dluschan@gmail.com>
2017-03-06 01:28:04 +0300

    для удобства перебора коэффициенты хранятся вместе со списками значений

--- vseros/2014-15/fin/auto_friends.py
+++ vseros/2014-15/fin/auto_friends.py
@@ -1,34 +1,25 @@
-a = [0 for x in range(100)]
-b = [0 for x in range(100)]
-c = [0 for x in range(100)]
-ab = [0 for x in range(10000)]
-bc = [0 for x in range(10000)]
-ac = [0 for x in range(10000)]
-abc = [0 for x in range(1000000)]
+a = [[0 for x in range(100)], 1]
+b = [[0 for x in range(100)], 1]
+c = [[0 for x in range(100)], 1]
+ab = [[0 for x in range(10000)], -2]
+bc = [[0 for x in range(10000)], -2]
+ac = [[0 for x in range(10000)], -2]
+abc = [[0 for x in range(1000000)], 3]
 
 for i in range(int(input())):
     q, w, e = map(int, input().split())
-    a[q-1] += 1
-    b[w-1] += 1
-    c[e-1] += 1
-    ab[100*(q-1) + w-1] += 1
-    bc[100*(w-1) + e-1] += 1
-    ac[100*(q-1) + e-1] += 1
-    abc[10000*(q-1) + 100*(w-1) + e-1] += 1
+    a[0][q - 1] += 1
+    b[0][w - 1] += 1
+    c[0][e - 1] += 1
+    ab[0][100 * (q - 1) + (w - 1)] += 1
+    bc[0][100 * (w - 1) + (e - 1)] += 1
+    ac[0][100 * (q - 1) + (e - 1)] += 1
+    abc[0][10000 * (q - 1) + 100 * (w - 1) + (e - 1)] += 1
 
 couples = 0
-for records in [a, b, c]:
+for records, k in [a, b, c, ab, bc, ac, abc]:
     for value in records:
         if value > 1:
-            couples += value * (value - 1) // 2
-
-for records in [ab, bc, ac]:
-    for value in records:
-        if value > 1:
-            couples -= value * (value - 1)
-
-for value in abc:
-    if value > 1:
-        couples += 3 * (value * (value - 1) // 2)
+            couples += k * value * (value - 1) // 2
 
 print(couples)
