b5d4bdae14838370dce7fccf2e48afbd8cccbc7f
Dmitry Luschan <dluschan@gmail.com>
2018-10-20 19:21:55 +0300

    закончена задача «Собачки и миски»

--- technocup/2016/elimination_round_1/d.dogs_and_bowls.py
+++ technocup/2016/elimination_round_1/d.dogs_and_bowls.py
@@ -1,7 +1,11 @@
 n, m = map(int, input().split())
 dogs = sorted((int(x) for x in input().split()))
-full = 0
+done = 0
 for _ in range(m):
     x, t = map(int, input().split())
-    for 
-    [x - t, x + t]
+    for i, d in enumerate(dogs):
+        if x - t <= d <= x + t:
+            done += 1
+            dogs.pop(i)
+            break
+print(done)
